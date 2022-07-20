d4aeb031119e712c9ec0c58fe808d0568edd554a
“isolcat” <308750414@qq.com>
2022-07-20 17:37:06 +0800

    修改eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     extends: [
         'plugin:vue/vue3-essential',
         '@vue/standard',
-        '@vue/typescript/recommended'
+        '@vue/typescript/recommended',
+        'standard'
     ],
     parserOptions: {
         ecmaVersion: 2020
@@ -14,6 +15,7 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        "vue/multi-word-component-names": "off",
+        'vue/multi-word-component-names': 'off',
+        semi: 0
     }
 }
