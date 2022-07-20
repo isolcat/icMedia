515f6bdf23642eb74f1b0f6de50972ed6650986f
“isolcat” <308750414@qq.com>
2022-07-20 19:51:52 +0800

    引入less样式

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// @相当于src
+import '@/assets/styles/base.less'
+import '@/assets/styles/iconfont.less'
+
 createApp(App).use(router).mount('#app')
