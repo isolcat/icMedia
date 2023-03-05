08fd5727f5c2ef7aac06d19b1faa7e5f4138c8db
isolcat <76111612+isolcat@users.noreply.github.com>
2023-03-05 10:55:52 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { VantResolver } = require('unplugin-vue-components/resolvers');
 const ComponentsPlugin = require('unplugin-vue-components/webpack');
 
+
 module.exports = {
     configureWebpack: {
         plugins: [
